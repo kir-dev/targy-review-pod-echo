daa5c3bd033c2675f804d9f87f7bad85fd762c52
Sreter_Akos <sreterakos03@gmail.com>
2026-06-18 11:55:37 +0200

    SubjectDto class, enum separated

--- src/main/kotlin/hu/bme/sch/kirdev/targyreviewpodecho/subject/SubjectDto.kt
+++ src/main/kotlin/hu/bme/sch/kirdev/targyreviewpodecho/subject/SubjectDto.kt
@@ -1,4 +1,15 @@
 package hu.bme.sch.kirdev.targyreviewpodecho.subject
 
-class SubjectDto {
-}
+data class SubjectDto (
+    var id: Long? = null,
+
+    var name: String = "",
+
+    var semester: Int = 0,
+
+    var major: MajorType = MajorType.MERNOK_INFO,
+
+    var bmeId: String = "",
+
+    var description: String = ""
+)

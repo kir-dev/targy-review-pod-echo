da3c6d2570180062b263a4011fb2974f2f3a3c99
Tóth Zachary <231300066+zacharytoth@users.noreply.github.com>
2026-06-28 12:05:20 +0200

    teachermapper updateEntityFromDto

--- src/main/kotlin/hu/bme/sch/kirdev/targyreviewpodecho/teacher/TeacherMapper.kt
+++ src/main/kotlin/hu/bme/sch/kirdev/targyreviewpodecho/teacher/TeacherMapper.kt
@@ -2,17 +2,21 @@ package hu.bme.sch.kirdev.targyreviewpodecho.teacher
 
 class TeacherMapper {
     fun entityToDto(entity: TeacherEntity): TeacherDto {
-        //subject needed
         return TeacherDto(
             id = entity.id,
             name = entity.name ?: ""
+            //subject needed
         )
     }
 
     fun dtoToEntity(dto: TeacherDto): TeacherEntity {
-        //subject needed
         return TeacherEntity(
             name = dto.name ?: ""
+            //subject needed
         )
+    }
+    fun updateEntityFromDto(dto: TeacherDto, entity: TeacherEntity) {
+        entity.name = dto.name
+        //subject needed
     }
 }
